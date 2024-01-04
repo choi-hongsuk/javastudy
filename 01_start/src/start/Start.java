ba9f015b0d6f66d2916e78575709f93b77c5170c
choi-hongsuk <dkdlfktkd@naver.com>
2024-01-04 10:21:13 +0900

    [JAVA]1일차

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,25 @@
+package start;
+
+public class Start {
+	
+	public static void main(String[] args) {
+	
+	// single-Line comment	
+	
+	/*
+	  multi-Line comment
+	 */	
+		
+	/**
+	 * documentation comment
+	 * 작성자, 작성일자, 설명등을 API 문서로 제공하기 위한 주석
+	 */
+		
+		//출력문
+		System.out.println(1);  		// println() : 출력 후 줄 바꿈까지 해 주낟.
+		System.out.print(2);			// print()   : 출력만 한다.
+		System.out.printf("%d", 3);		// printf()  : 포멧 문자를 기반으로 출력한다.
+	
+	}
+}
+
---
+++ 02_variable/src/pkg01_variable/MainClass.java
@@ -0,0 +1,10 @@
+package pkg01_variable;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
