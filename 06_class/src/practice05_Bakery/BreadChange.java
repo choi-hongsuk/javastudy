186190eeb6bb9953385911c7f258aa480801f643
choi-hongsuk <dkdlfktkd@naver.com>
2024-01-12 12:39:13 +0900

    20240112

--- 06_class/src/practice05_Bakery/BreadChange.java
+++ 06_class/src/practice05_Bakery/BreadChange.java
@@ -1,38 +1,33 @@
 package practice05_Bakery;
 
 public class BreadChange {
-	
-	// field
-	private int bread;		// 빵
-	private int change;		// 잔돈
-	
-	// constructor
-	public BreadChange() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public BreadChange(int bread, int change) {
-		super();
-		this.bread = bread;
-		this.change = change;
-	}
+  // field
+  private int bread;   // 빵
+  private int change;  // 잔돈
+  
+  // constructor
+  public BreadChange() {
 
-	public int getBread() {
-		return bread;
-	}
-
-	public void setBread(int bread) {
-		this.bread = bread;
-	}
-
-	public int getChange() {
-		return change;
-	}
-
-	public void setChange(int change) {
-		this.change = change;
-	}
-	
-	
-	
+  }
+  public BreadChange(int bread, int change) {
+    super();
+    this.bread = bread;
+    this.change = change;
+  }
+  
+  // method
+  public int getBread() {
+    return bread;
+  }
+  public void setBread(int bread) {
+    this.bread = bread;
+  }
+  public int getChange() {
+    return change;
+  }
+  public void setChange(int change) {
+    this.change = change;
+  }
+  
 }
